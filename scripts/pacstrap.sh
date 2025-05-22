a297b5d279b7430dd645631e881a6acf2c059ad6
xdawxd <xrealz110@gmail.com>
2025-05-22 09:50:47 +0200

    fix nvidia envs,add thefuck alias and fastfetch to pacstrap

--- scripts/pacstrap.sh
+++ scripts/pacstrap.sh
@@ -1,1 +1,1 @@
-pacstrap -K /mnt base linux linux-firmware intel-ucode networkmanager nano man-db man-pages text-info git openssh
+pacstrap -K /mnt base linux linux-firmware intel-ucode networkmanager nano man-db man-pages text-info git openssh fastfetch
