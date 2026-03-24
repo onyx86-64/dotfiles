b9664b6e538b354ee49726b326623a1ddb111b22
onyx86-64 <xrealz110@gmail.com>
2026-03-24 09:52:41 +0100

    adjust noice.lua to handle only shell out/err

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,18 +1,21 @@
 return {
-  {
-    "folke/noice.nvim",
-    opts = function(_, opts)
-      opts.routes = opts.routes or {}
+	{
+		"folke/noice.nvim",
+		opts = function(_, opts)
+			opts.routes = opts.routes or {}
 
-      table.insert(opts.routes, {
-        filter = { event = "msg_show" },
-        view = "notify",
-        opts = {
-          level = "info",
-          skip = false,
-          replace = false,
-        },
-      })
-    end,
-  },
+			table.insert(opts.routes, {
+				filter = {
+					event = "msg_show",
+					kind = { "shell_out", "shell_err" },
+				},
+				view = "split",
+				opts = {
+					level = "info",
+					skip = false,
+					replace = false,
+				},
+			})
+		end,
+	},
 }
